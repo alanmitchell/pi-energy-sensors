b7c679485140a65376eb57cb02b2fa0dc42bb265
Alan Mitchell <tabb99@gmail.com>
2017-04-19 19:16:07 -0800

    Started BTU Meter script.

---
+++ btu_meter.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python
+"""Script to implement a BTU meter that uses a pulse output flow meter
+and two thermistors to measure hydronic heat flow.  Results are posted
+to the mini-monitor MQTT broker.
+
+This script should be started by a supervisor capable of restarting
+the script if an error occurs.
+"""
+import time
+import sys
+import argparse
+import input_change
+import mqtt_poster
+
+# GPIO Pins (BCM numbering) used by the pulse counter
+PIN_PULSE_IN = 13
+PIN_DEBUG = 5
+
+# Count at which counter rolls to zero
+PULSE_ROLLOVER = 1000000
+
+# process command line arguments
+parser = argparse.ArgumentParser(description='BTU Meter Script.')
+parser.add_argument("-d", "--debug", help="turn on Debug pin", action="store_true")
+parser.add_argument("-b", "--both", help="count both low-to-high and high-to-low transitions", 
+        action="store_true")
+args = parser.parse_args()
+
+# set the debug pin if requested
+debug_pin = PIN_DEBUG if args.debug else None
+
+# flag to determine if both transitions are counted
+count_both = args.both
+
+# Access some settings in the Mini-Monitor settings file
+# The settings file is installed in the FAT boot partition of the Pi SD card,
+# so that it can be easily configured from the PC that creates the SD card.  
+# Include that directory in the Path so the settings file can be found.
+sys.path.insert(0, '/boot/pi_logger')
+import settings
+
+# make a sensor id
+sensor_id = '%s_%2d_btu' % (settings.LOGGER_ID, PIN_PULSE_IN)
+
+# get logging interval in seconds
+log_interval = settings.LOG_INTERVAL
+
+# start up the object that posts to the MQTT broker
+poster = mqtt_poster.MQTTposter()
+poster.start()
+
+pulse_count = 0
+
+def chg_detected(pin_num, new_state):
+    """This is called when the input pin changes state.
+    """
+    global pulse_count
+
+    if new_state:
+        # always count low-to-high transition
+        pulse_count += 1
+    elif count_both:
+        # only count high-to-low if requested
+        pulse_count += 1
+    pulse_count = pulse_count % PULSE_ROLLOVER
+
+# Start up the Input Pin Change Detector
+# I have a 10 K pull-up on the board and a 0.01 uF cap to ground.
+chg_detect = input_change.InputChange(PIN_PULSE_IN, chg_detected, pull_up=False, debug_pin=debug_pin)
+chg_detect.start()
+
+# determine time to log count
+next_log_ts = time.time() + log_interval
+
+while True:
+
+    if not chg_detect.isAlive():
+        # change detector is not running.  Exit with an error
+        sys.exit(1)
+
+    ts = time.time()
+    if ts > next_log_ts:
+        poster.publish('readings/final/btu_meter', '%s\t%s\t%s' % (int(ts), sensor_id, pulse_count))
+        next_log_ts += log_interval
+
+    time.sleep(0.5)
