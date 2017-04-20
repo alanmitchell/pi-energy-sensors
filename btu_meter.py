c16a9407cac579dbac8e2acd99c6e2bfe40c9daf
Alan Mitchell <tabb99@gmail.com>
2017-04-19 19:41:31 -0800

    Added Calibrate button functionality.

--- btu_meter.py
+++ btu_meter.py
@@ -11,13 +11,26 @@ import sys
 import argparse
 import input_change
 import mqtt_poster
+import RPi.GPIO as GPIO
 
-# GPIO Pins (BCM numbering) used by the pulse counter
+# GPIO Pins (BCM numbering) and ADC channels used by the BTU METER
 PIN_PULSE_IN = 13
+PIN_CALIBRATE = 4
+PIN_LED = 16
 PIN_DEBUG = 5
+ADC_CH_THOT = 0
+ADC_CH_TCOLD = 1
 
-# Count at which counter rolls to zero
+# Make the LED pin an output
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(PIN_LED, GPIO.OUT)
+
+# Count at which pulse counter rolls to zero
 PULSE_ROLLOVER = 1000000
+
+# Value at which the heat count rolls over.  This is a sum of the delta-Ts
+# that are present when pulses occur.
+HEAT_ROLLOVER = 1000000.0
 
 # process command line arguments
 parser = argparse.ArgumentParser(description='BTU Meter Script.')
@@ -56,17 +69,27 @@ def chg_detected(pin_num, new_state):
     """
     global pulse_count
 
-    if new_state:
-        # always count low-to-high transition
-        pulse_count += 1
-    elif count_both:
-        # only count high-to-low if requested
-        pulse_count += 1
-    pulse_count = pulse_count % PULSE_ROLLOVER
+    if pin_num == PIN_PULSE_IN:
+        if new_state:
+            # always count low-to-high transition
+            pulse_count += 1
+        elif count_both:
+            # only count high-to-low if requested
+            pulse_count += 1
+        pulse_count = pulse_count % PULSE_ROLLOVER
+
+    elif pin_num == PIN_CALIBRATE:
+        if new_state == False:
+            time.sleep(1.0)
+            if GPIO.input(PIN_CALIBRATE)==False:
+                GPIO.output(PIN_LED, True)
+                time.sleep(1.0)
+                GPIO.output(PIN_LED, False)
+                # perform calibration here
 
 # Start up the Input Pin Change Detector
 # I have a 10 K pull-up on the board and a 0.01 uF cap to ground.
-chg_detect = input_change.InputChange(PIN_PULSE_IN, chg_detected, pull_up=False, debug_pin=debug_pin)
+chg_detect = input_change.InputChange([PIN_PULSE_IN, PIN_CALIBRATE], chg_detected, pull_up=False, debug_pin=debug_pin)
 chg_detect.start()
 
 # determine time to log count
