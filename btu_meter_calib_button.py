d9c6c8050aef1252e105c51bd7f7b966ffa62b00
Alan Mitchell <tabb99@gmail.com>
2017-05-07 16:31:05 -0800

    Made main script use Calibrate Values from Settings file.
    Renamed old script that used the calibrate button.

--- btu_meter_calib_button.py
+++ btu_meter_calib_button.py
@@ -3,11 +3,6 @@
 and two thermistors to measure hydronic heat flow.  Results are posted
 to the mini-monitor MQTT broker.
 
-This version does not use a Calibrate button to null out any error in the
-measured delta-T.  If you need that feature, see 'btu_meter_calib_button.py'.
-Instead, this version pulls calibration values for the two temperature sensors
-out of the settings file.
-
 This script should be started by a supervisor capable of restarting
 the script if an error occurs.
 
@@ -30,12 +25,18 @@ import Adafruit_MCP3008
 
 # GPIO Pins (BCM numbering) and ADC channels used by the BTU METER
 PIN_PULSE_IN = 13      # the pulse input pin
+PIN_CALIBRATE = 4      # the calibrate push button is connected here
+PIN_LED = 16           # the calibrate LED is on this pin
 PIN_DEBUG = 5          # a pin used to output a debug signal
 ADC_CH_THOT = 0        # the MCP3008 ADC channel used for the hot thermistor
 ADC_CH_TCOLD = 1       # the MCP3008 ADC channel used for the cold thermistor
 
 # Eliminate GPIO warnings
 GPIO.setwarnings(False)
+
+# Make the LED pin an output
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(PIN_LED, GPIO.OUT)
 
 # Count at which pulse counter rolls to zero
 PULSE_ROLLOVER = 1000000
@@ -47,6 +48,9 @@ HEAT_ROLLOVER = 1000000.0
 # Number of A/D readings to average together to get the 
 # current temperature reading.
 BUF_LEN_TEMP = 100
+
+# Path to temperature calibration file
+PATH_CALIBRATE = '/var/local/btu_calibration'
 
 # process command line arguments
 parser = argparse.ArgumentParser(description='BTU Meter Script.')
@@ -70,16 +74,10 @@ base_sensor_id = '%s_%2d_btu' % (settings.LOGGER_ID, PIN_PULSE_IN)
 log_interval = getattr(settings, 'BTU_LOG_INTERVAL', 10 * 60)
 
 # get the minimum delta-T required to tally energy flow (deg F)
-min_delta_T = getattr(settings, 'BTU_MIN_DELTA_T', 0.0)
+min_delta_T = getattr(settings, 'BTU_MIN_DELTA_T', 0.2)
 
 # flag to determine if both transitions are counted
 count_both = getattr(settings, 'BTU_BOTH_EDGES', False)
-
-# Calibration value to add to the Hot Temperature reading
-calibrate_hot = getattr(settings, 'CALIBRATE_ADJ_HOT', 0.0)
-
-# Calibration value to add to the Cold Temperature reading
-calibrate_cold = getattr(settings, 'CALIBRATE_ADJ_COLD', 0.0)
 
 # start up the object that posts to the MQTT broker
 poster = mqtt_poster.MQTTposter()
@@ -109,20 +107,35 @@ mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 ad_hot = [mcp.read_adc(ADC_CH_THOT)] * BUF_LEN_TEMP
 ad_cold = [mcp.read_adc(ADC_CH_TCOLD)] * BUF_LEN_TEMP
 
-def current_temps():
+# read in the temperature calibration coefficients if the file exists
+if os.path.exists(PATH_CALIBRATE):
+    with open(PATH_CALIBRATE, 'r') as fin:
+        calibrate_hot = float(fin.readline())
+        calibrate_cold = float(fin.readline())
+else:
+    calibrate_hot = 0.0
+    calibrate_cold = 0.0
+
+def current_temps(include_calibration=True):
     """Returns the current hot and cold temperatures, averaging the values
-    in the reading buffer.  Always applies the calibration value.
+    in the reading buffer.  If 'include_calibration' is True, apply the
+    calibration values.
     """
     thot = sum(ad_hot)/float(BUF_LEN_TEMP)
-    thot = therm.TfromV(thot) + calibrate_hot
+    thot = therm.TfromV(thot)
+    if include_calibration:
+        thot += calibrate_hot
     tcold = sum(ad_cold)/float(BUF_LEN_TEMP)
-    tcold = therm.TfromV(tcold) + calibrate_cold
+    tcold = therm.TfromV(tcold)
+    if include_calibration:
+        tcold += calibrate_cold
     return thot, tcold
 
 def chg_detected(pin_num, new_state):
     """This is called when any of watched input pins change state.
     """
     global pulse_count, heat_count
+    global calibrate_hot, calibrate_cold
 
     if pin_num == PIN_PULSE_IN:
         if new_state == False or count_both:
@@ -138,9 +151,32 @@ def chg_detected(pin_num, new_state):
             heat_count += delta_T
             heat_count = heat_count % HEAT_ROLLOVER
 
+    elif pin_num == PIN_CALIBRATE:
+        if new_state == False:
+            # Calibrate button was pressed
+            # Check a second later to see if it is still pressed.
+            # Note that this interrupts pulse counting.
+            time.sleep(1.0)
+            if GPIO.input(PIN_CALIBRATE)==False:
+                # blink LED to indicate that calibrate function will occur
+                GPIO.output(PIN_LED, True)
+                time.sleep(1.0)
+                GPIO.output(PIN_LED, False)
+
+                # calibrate process
+                thot, tcold = current_temps(include_calibration=False)
+                true_temp = (thot + tcold)/2.0  # deemed the true temp
+                calibrate_hot = true_temp - thot
+                calibrate_cold = true_temp - tcold
+                # backup old calibration values and store new values
+                shutil.copy(PATH_CALIBRATE, PATH_CALIBRATE + '.bak')
+                with open(PATH_CALIBRATE, 'w') as fout:
+                    fout.write('%s\n' % calibrate_hot)
+                    fout.write('%s\n' % calibrate_cold)
+
 # Start up the Input Pin Change Detector
 # I have a 10 K pull-up on the board and a 0.01 uF cap to ground.
-chg_detect = input_change.InputChange([PIN_PULSE_IN], chg_detected, pull_up=False, debug_pin=debug_pin)
+chg_detect = input_change.InputChange([PIN_PULSE_IN, PIN_CALIBRATE], chg_detected, pull_up=False, debug_pin=debug_pin)
 chg_detect.start()
 
 # determine time to log count
